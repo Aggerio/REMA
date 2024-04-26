47b5febf52fad3d36e78507db6a101890333ff96
Gourav <k.gourav254@gmail.com>
2024-04-26 19:52:40 +0530

    form shit change

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 function Form(){
     return (
-        <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-100 '>
-            <h1 className='text-7xl'>Hola!</h1>
+        <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-100'>
+            <h1 className='text-7xl'>Hello!</h1>
             <h3 className='text-3xl font-semibold mt-9'>Welcome Back</h3>
             <p className='font-medium text-lg text-gray-500 mt-4'>Please enter your details.</p>
             <div className='mt-8'>
@@ -24,20 +24,20 @@ function Form(){
                     />
                 </div>
                 <div className='mt-8 justify-between items-center'>
-                    <button className='font-medium text-base text-violet-500'>Forgot password?</button>
+                    <button className='font-medium text-base text-[#EA7C69]'>Forgot password?</button>
                 </div>
                 <div className='mt-8 flex flex-col gap-y-4'>
-                    <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-violet-500 text-white text-lg font-bold'>Sign In</button>
+                    <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-[#EA7C69] text-white text-lg font-bold'>Sign In</button>
                     <button className='flex rounded-xl py-3 border-2 border-gray-100 items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all'>
-                        <svg width="24" height="24" viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        {/* <svg width="24" height="24" viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
                             
-                        </svg>
+                        </svg> */}
                         Sign in with Google
                         </button>
                 </div>
                 <div className='mt-8 flex justify-center items-center'>
                     <p className='font-medium text-base'>Don't have an account?</p>
-                    <button className='text-violet-500 text-base font-medium ml-2'>Sign Up</button>
+                    <button className='text-[#EA7C69] text-base font-medium ml-2'>Sign Up</button>
                 </div>
             </div>
         </div>
