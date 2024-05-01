f747c5abd3317e65107d87c2ccfc7c1b6c6652a0
Aggerio <agamsrivastava004@gmail.com>
2024-05-01 13:40:16 +0530

    backend ahoy

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/profile', async (req, res) => {
   try {
     //res.json(books);
   } catch (err) {
