6b499a4cbefbe54c133a61e2e5dbdb3f8343dbd8
Gourav <k.gourav254@gmail.com>
2024-05-02 00:52:55 +0530

    restaurant page fixed

--- frontend/src/components/RestaurantCard.jsx
+++ frontend/src/components/RestaurantCard.jsx
@@ -11,7 +11,7 @@ export default function RestaurantCard({
 
   const navigate = useNavigate();
   return (
-    <div className="relative min-w-[20vw] min-h-[10vh] max-w-[20%] max-h-[15%] " onClick={() => {navigate(`/restaurant/${rest_id}`)}}>
+    <div className="relative min-w-[20vw] min-h-[10vh] max-w-[20%] max-h-[15%] m-3" onClick={() => {navigate(`/restaurant/${rest_id}`)}}>
       <img className="object-contain rounded-[10%] absolute left-0 top-0"  src={rest_img} />
       <div className="text-center bg-[#1F1D2B] rounded-[10%] pt-[70%] pb-[10%] pr-[5%] pl-[5%]">
         <p className="text-white">{rest_name}</p>
--- frontend/src/pages/AllRestaurants.jsx
+++ frontend/src/pages/AllRestaurants.jsx
@@ -7,31 +7,68 @@ export default function AllRestaurants() {
       <div>
         <CentralNavbar />
       </div>
-      <div className="ml-[10%] mt-[2%]">
-        <div className="flex">
+      <div className="ml-[10%] mt-[2%] w-[85%]">
+        <div className="flex justify-between">
           <div className="flex-1">
             <h1 className="text-[2rem] text-white">Hi Agam,</h1>
             <p className="text-white mt-3">Friday 26 April, 2024</p>
           </div>
-          <input className="bg-[#252836] rounded-[10%] text-white text-[1rem] text-center" type="text" name="search" onChange = {() => {}} value={"🔍 Search for food"} />
+          <div>
+            <input
+              className="bg-[#252836] rounded-[10%] text-white text-[1rem] text-center h-[4rem]"
+              type="text"
+              name="search"
+              onChange={() => {}}
+              value={"🔍 Search for food"}
+            />
+          </div>
         </div>
-        <div>
-          <RestaurantCard
-            rest_id="1"
-            rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
-            rest_name="Vellore Restaurant 1"
-            rest_distance="0.8km"
-            rest_rating="4 stars"
-          />
-        </div>
-        <div>
-          <RestaurantCard
-            rest_id="1"
-            rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
-            rest_name="Vellore Restaurant 1"
-            rest_distance="0.8km"
-            rest_rating="4 stars"
-          />
+        <div className="grid grid-flow-row grid-cols-3">
+          <div>
+            <RestaurantCard
+              rest_id="1"
+              rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
+              rest_name="Vellore Restaurant 1"
+              rest_distance="0.8km"
+              rest_rating="4 stars"
+            />
+          </div>
+          <div>
+            <RestaurantCard
+              rest_id="1"
+              rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
+              rest_name="Vellore Restaurant 1"
+              rest_distance="0.8km"
+              rest_rating="4 stars"
+            />
+          </div>
+          <div>
+            <RestaurantCard
+              rest_id="1"
+              rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
+              rest_name="Vellore Restaurant 1"
+              rest_distance="0.8km"
+              rest_rating="4 stars"
+            />
+          </div>
+          <div>
+            <RestaurantCard
+              rest_id="1"
+              rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
+              rest_name="Vellore Restaurant 1"
+              rest_distance="0.8km"
+              rest_rating="4 stars"
+            />
+          </div>
+          <div>
+            <RestaurantCard
+              rest_id="1"
+              rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
+              rest_name="Vellore Restaurant 1"
+              rest_distance="0.8km"
+              rest_rating="4 stars"
+            />
+          </div>
         </div>
       </div>
     </div>
