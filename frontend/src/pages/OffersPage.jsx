9928f57bb16f7439c6b35e13e95af4e553672a9b
Aggerio <agamsrivastava004@gmail.com>
2024-05-01 22:37:43 +0530

    started completion of frontend

--- frontend/src/pages/OffersPage.jsx
+++ frontend/src/pages/OffersPage.jsx
@@ -13,25 +13,6 @@ export default function OfferPage()
             <h1 className="text-[2rem] text-white">Hi Agam,</h1>
             <p className="text-white mt-3">Friday 26 April, 2024</p>
           </div>
-          <input className=" bg-[#252836] rounded-[10%] text-white text-[1rem] text-center" type="text" name="search" value={"🔍 Search for food"} />
-        </div>
-        <div>
-          <RestaurantCard
-            rest_id="1"
-            rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
-            rest_name="Vellore Restaurant 1"
-            rest_distance="0.8km"
-            rest_rating="4 stars"
-          />
-        </div>
-        <div>
-          <RestaurantCard
-            rest_id="1"
-            rest_img="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
-            rest_name="Vellore Restaurant 1"
-            rest_distance="0.8km"
-            rest_rating="4 stars"
-          />
         </div>
       </div>
     </div>
