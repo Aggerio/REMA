e8d9eb94393ae151e08c45f239b760b455e8e5ed
Aggerio <agamsrivastava004@gmail.com>
2024-05-03 15:24:04 +0530

    fixed logged out

--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
     name: "user",
     initialState: {
-       role : "",
+       role : "restaurant",
     },
     reducers: {
     change_user_role: (state, action) => {
